b9996c361263a5ef53d7b1d698d4499fb139a4fb
Dr. Christian Nöding <christian@noeding-online.de>
2023-08-12 22:58:47 +0200

    Changed handling of detection of channel 1 by using Block-Sync-signal

--- i2s_ultranet_rx.vhd
+++ i2s_ultranet_rx.vhd
@@ -1,33 +1,44 @@
+-------------------------------------------------------------------------------
+-- Receiver for Behringer UltraNet
+-- based on OpenCore's i2s_interface
+--
+-- UltraNet sends eight 24-bit audio-samples in AES/EBU-format.
+-- this file expects I2S-data converted from AES/EBU
+--
+-- bsync (Z subframe in AES/EBU) will signal channel 1 followed by seven more samples
+-- individual samples are identified by lrclk
+------------------------------------------------------------------------------- 
+
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all; 
+use ieee.numeric_std.all; -- lib for unsigned and signed
 
 entity i2s_ultranet_rx is
-	generic ( BITPERFRAME : integer := 32 -- ultranet uses 24bit for audio-data + 8bit frame-data
-    		);
 	port (
-			clk 		: in std_logic;
-			bsync		: in std_logic;
-         bclk 		: in std_logic;
-         sdata		: in std_logic;
-         lrclk		: in std_logic;
-         sample_out	: out std_logic_vector(23 downto 0);
-			frame_out	: out std_logic_vector(7 downto 0);
-         new_data	: out std_logic
+			clk 		: in std_logic; -- Master clock
+			bsync		: in std_logic; -- Block start (asserted when Z subframe is being transmitted)
+         bclk 		: in std_logic; -- output serial data clock (AES3-clock)
+         sdata		: in std_logic; -- output serial data
+         lrclk		: in std_logic; -- Frame sync (asserted for channel A, negated for B)
+			
+         sample_out	: out std_logic_vector(23 downto 0); -- received audio-sample
+         channel	: out unsigned(7 downto 0); -- received channel-number
+         new_data	: out std_logic -- new data received successfully
         );
 end i2s_ultranet_rx;
 
 architecture rtl of i2s_ultranet_rx is
 	signal sample_data			: std_logic_vector(23 downto 0);
-	signal frame_data				: std_logic_vector(7 downto 0);
 	signal neg_edge, pos_edge 	: std_logic;
 	signal lr_edge					: std_logic;
 	signal rx_sampledata 		: std_logic;
-	signal rx_framedata			: std_logic;
 
 	signal zbclk, zzbclk, zzzbclk 	: std_logic;
 	signal zlrclk, zzlrclk, zzzlrclk: std_logic;
 	signal cnt						: integer range 0 to 31 := 0;
-
+	
+	signal chn_cnt					: integer range 0 to 8 := 1;
 begin
 	detect_edge : process(clk)
 	begin
@@ -63,30 +74,28 @@ begin
 	detect_sample : process(clk)
 	begin
 		if rising_edge(clk) then
-			if bsync = '1' then 
+			if bsync = '1' then -- begin of new block (means channel 1 on Ultranet)
 				cnt <= 0;
+				chn_cnt <= 1; -- reset channel-counter to 1
 				new_data <= '0';
 			else
-				if lr_edge = '1' then
+				if lr_edge = '1' then -- rising or falling edge on LRCLK detected
 					cnt <= 0;
+					chn_cnt <= chn_cnt + 1; -- increase channel-counter on each LRCLK-edge
 				end if;
 				if pos_edge = '1' then
-					if cnt < BITPERFRAME - 1 then
-						cnt <= cnt + 1;
+					if cnt < 24 + 1 then
+						cnt <= cnt + 1; -- increment cnt until 25
 					end if;
 				end if;
 				if neg_edge = '1' then  	
 					if cnt = 1 then -- begin of sampledata
 						rx_sampledata <= '1';
-						rx_framedata <= '0';
-					elsif cnt > 24 and cnt < BITPERFRAME then -- reached end of sampledata, begin of framedata
+					elsif cnt = 24 + 1 then -- reached end of sampledata
 						rx_sampledata <= '0';
-						rx_framedata <= '1';
-					elsif cnt = BITPERFRAME then -- reached end of framedata
-						rx_framedata <= '0';
 					end if;
 				end if;
-				if cnt = BITPERFRAME + 1 and neg_edge = '1' then -- reached end of sampledata
+				if cnt = 24 + 1 and neg_edge = '1' then -- reached end of sampledata
 					new_data <= '1';
 				else
 					new_data <= '0';
@@ -96,7 +105,7 @@ begin
 	end process;
 
 	sample_out <= sample_data;
-	frame_out <= frame_data;
+	channel <= to_unsigned(chn_cnt, channel'length);
 
 	get_data : process(clk)
 	begin
@@ -104,11 +113,6 @@ begin
 			-- receive individual bits for audio-data (24 bits)
 			if pos_edge = '1' and rx_sampledata = '1' then
 				sample_data <= sample_data(sample_data'high - 1 downto 0) & sdata;
-			end if;
-			
-			-- receive individual bits for frame-data (8 bits)
-			if pos_edge = '1' and rx_framedata = '1' then
-				frame_data <= frame_data(frame_data'high -1 downto 0) & sdata;
 			end if;
 		end if;
 	end process;
--- ultranet_demux.vhd
+++ ultranet_demux.vhd
@@ -1,12 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all; -- lib for unsigned and signed
 
 entity ultranet_demux is
 	port
 	(
-		clk				: in std_logic;
-		sample_data		: in std_logic_vector(23 downto 0);
-		frame_data		: in std_logic_vector(7 downto 0);
+		sample_in		: in std_logic_vector(23 downto 0);
+		channel			: in unsigned(7 downto 0);
 		process_data	: in std_logic;
 		
 		ch1_out 			: out std_logic_vector(23 downto 0);
@@ -21,38 +22,27 @@ entity ultranet_demux is
 end entity;
 
 architecture rtl of ultranet_demux is
-	signal channel					: integer range 1 to 8 := 1; -- channel-counter
 begin
-	process(clk)
-		constant multiframe_sync	: std_logic_vector(7 downto 0):= "00001001"; -- this will mark channel 1
-		constant midframe 			: std_logic_vector(7 downto 0):= "00000001"; -- this will mark channel 2 to 8
+	process(process_data)
 	begin
-		if (rising_edge(clk)) then
-			if process_data = '1' then
-				if frame_data = multiframe_sync then -- receive channel 1
-					channel <= 1;
-				else -- receive channel 2..8
-					channel <= channel + 1;
-				end if;
-				
-				-- store individual channels to output-vectors
-				if channel = 1 then
-					ch1_out <= sample_data;
-				elsif channel = 2 then
-					ch2_out <= sample_data;
-				elsif channel = 3 then
-					ch3_out <= sample_data;
-				elsif channel = 4 then
-					ch4_out <= sample_data;
-				elsif channel = 5 then
-					ch5_out <= sample_data;
-				elsif channel = 6 then
-					ch6_out <= sample_data;
-				elsif channel = 7 then
-					ch7_out <= sample_data;
-				elsif channel = 8 then
-					ch8_out <= sample_data;
-				end if;
+		if (rising_edge(process_data)) then
+			-- store individual channels to output-vectors
+			if channel = 1 then
+				ch1_out <= sample_in;
+			elsif channel = 2 then
+				ch2_out <= sample_in;
+			elsif channel = 3 then
+				ch3_out <= sample_in;
+			elsif channel = 4 then
+				ch4_out <= sample_in;
+			elsif channel = 5 then
+				ch5_out <= sample_in;
+			elsif channel = 6 then
+				ch6_out <= sample_in;
+			elsif channel = 7 then
+				ch7_out <= sample_in;
+			elsif channel = 8 then
+				ch8_out <= sample_in;
 			end if;
 		end if;
 	end process;

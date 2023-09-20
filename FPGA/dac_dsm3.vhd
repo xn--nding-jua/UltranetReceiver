f2df1f2c862c56a7ffb4e60e423016c28eb950ad
Dr. Christian Nöding <christian@noeding-online.de>
2023-09-20 21:25:40 +0200

    New DAC: Added PDM-output-modulator with 4.8MHz-clock to achieve 50x decimation-ratio to get fs/2=48kHz audio.

---
+++ FPGA/dac_dsm3.vhd
@@ -0,0 +1,83 @@
+-------------------------------------------------------------------------------
+-- Title      : DAC_DSM2 - sigma-delta DAC converter with double loop
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : dac_dsm2.vhd
+-- Author     : Wojciech M. Zabolotny ( wzab[at]ise.pw.edu.pl )
+-- Company    : 
+-- Created    : 2009-04-28
+-- Last update: 2012-10-16
+-- Platform   : 
+-- Standard   : VHDL'93c
+-------------------------------------------------------------------------------
+-- Description: Implementation without variables
+-------------------------------------------------------------------------------
+-- Copyright (c) 2009 - THIS IS PUBLIC DOMAIN CODE!!!
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2009-04-28  1.0      wzab    Created
+-------------------------------------------------------------------------------
+-- There are two implementations:
+-- dsm2 - allows to obtain higher clock frequency, and therefore 
+--        higher oversampling ratio, but number of rising and falling
+--        slopes in time unit depends on signal value. Therefore
+--        you may experience nonlinear distortions if those two slopes
+--        are not symmetrical.
+-- dsm3 - The output of the DAC is updated once every three clock pulses.
+--        If there is a '1' on the DAC output, the sequence '110' is generated
+--        on the dout output. If there is a '0' on the DAC output, the sequence
+--        '100' is generated. Therefore we always have one rising slope and one 
+--        falling slope generated in each DAC cycle.
+--        Unfortunately this implementation accepts lower clock frequencies,
+--        so the oversampling ratio is lower
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity dac_dsm3 is
+  
+  generic (
+    nbits : integer := 24);
+
+  port (
+    din     : in  signed((nbits-1) downto 0);
+    dout    : out std_logic;
+    clk     : in  std_logic;
+    clk_ena : in  std_logic;
+    n_rst   : in  std_logic);
+
+end dac_dsm3;
+
+architecture beh1 of dac_dsm3 is
+
+  signal del1, del2, d_q : signed(nbits+2 downto 0) := (others => '0');
+  constant c1            : signed(nbits+2 downto 0) := to_signed(1, nbits+3);
+  constant c_1           : signed(nbits+2 downto 0) := to_signed(-1, nbits+3);
+begin  -- beh1
+
+  process (clk, n_rst)
+  begin  -- process
+    if n_rst = '0' then                 -- asynchronous reset (active low)
+      del1 <= (others => '0');
+      del2 <= (others => '0');
+      dout <= '0';
+    elsif clk'event and clk = '1' then  -- rising clock edge
+      if clk_ena = '1' then
+        del1 <= din - d_q + del1;
+        del2 <= din - d_q + del1 - d_q + del2;
+        if din - d_q + del1 - d_q + del2 > 0 then
+          d_q  <= shift_left(c1, nbits);
+          dout <= '1';
+        else
+          d_q  <= shift_left(c_1, nbits);
+          dout <= '0';
+        end if;
+      end if;
+    end if;
+  end process;
+  
+
+end beh1;
--- FPGA/pll_2.vhd
+++ FPGA/pll_2.vhd
@@ -44,7 +44,8 @@ ENTITY pll_2 IS
 	PORT
 	(
 		inclk0		: IN STD_LOGIC  := '0';
-		c0		: OUT STD_LOGIC 
+		c0		: OUT STD_LOGIC ;
+		c1		: OUT STD_LOGIC 
 	);
 END pll_2;
 
@@ -57,6 +58,7 @@ ARCHITECTURE SYN OF pll_2 IS
 	SIGNAL sub_wire2	: STD_LOGIC_VECTOR (0 DOWNTO 0);
 	SIGNAL sub_wire3	: STD_LOGIC_VECTOR (4 DOWNTO 0);
 	SIGNAL sub_wire4	: STD_LOGIC ;
+	SIGNAL sub_wire5	: STD_LOGIC ;
 
 
 
@@ -67,6 +69,10 @@ ARCHITECTURE SYN OF pll_2 IS
 		clk0_duty_cycle		: NATURAL;
 		clk0_multiply_by		: NATURAL;
 		clk0_phase_shift		: STRING;
+		clk1_divide_by		: NATURAL;
+		clk1_duty_cycle		: NATURAL;
+		clk1_multiply_by		: NATURAL;
+		clk1_phase_shift		: STRING;
 		compensate_clock		: STRING;
 		inclk0_input_frequency		: NATURAL;
 		intended_device_family		: STRING;
@@ -128,8 +134,10 @@ BEGIN
 	sub_wire2    <= To_stdlogicvector(sub_wire2_bv);
 	sub_wire0    <= inclk0;
 	sub_wire1    <= sub_wire2(0 DOWNTO 0) & sub_wire0;
+	sub_wire5    <= sub_wire3(1);
 	sub_wire4    <= sub_wire3(0);
 	c0    <= sub_wire4;
+	c1    <= sub_wire5;
 
 	altpll_component : altpll
 	GENERIC MAP (
@@ -138,6 +146,10 @@ BEGIN
 		clk0_duty_cycle => 50,
 		clk0_multiply_by => 24,
 		clk0_phase_shift => "0",
+		clk1_divide_by => 20,
+		clk1_duty_cycle => 50,
+		clk1_multiply_by => 3,
+		clk1_phase_shift => "0",
 		compensate_clock => "CLK0",
 		inclk0_input_frequency => 31250,
 		intended_device_family => "Cyclone 10 LP",
@@ -171,7 +183,7 @@ BEGIN
 		port_scanread => "PORT_UNUSED",
 		port_scanwrite => "PORT_UNUSED",
 		port_clk0 => "PORT_USED",
-		port_clk1 => "PORT_UNUSED",
+		port_clk1 => "PORT_USED",
 		port_clk2 => "PORT_UNUSED",
 		port_clk3 => "PORT_UNUSED",
 		port_clk4 => "PORT_UNUSED",
@@ -217,8 +229,11 @@ END SYN;
 -- Retrieval info: PRIVATE: CUR_FBIN_CLK STRING "c0"
 -- Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "Any"
 -- Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
+-- Retrieval info: PRIVATE: DIV_FACTOR1 NUMERIC "1"
 -- Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
+-- Retrieval info: PRIVATE: DUTY_CYCLE1 STRING "50.00000000"
 -- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "6.144000"
+-- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE1 STRING "4.800000"
 -- Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 -- Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 -- Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -239,18 +254,26 @@ END SYN;
 -- Retrieval info: PRIVATE: LVDS_MODE_DATA_RATE STRING "Not Available"
 -- Retrieval info: PRIVATE: LVDS_MODE_DATA_RATE_DIRTY NUMERIC "0"
 -- Retrieval info: PRIVATE: LVDS_PHASE_SHIFT_UNIT0 STRING "deg"
+-- Retrieval info: PRIVATE: LVDS_PHASE_SHIFT_UNIT1 STRING "ps"
 -- Retrieval info: PRIVATE: MIG_DEVICE_SPEED_GRADE STRING "Any"
 -- Retrieval info: PRIVATE: MIRROR_CLK0 STRING "0"
+-- Retrieval info: PRIVATE: MIRROR_CLK1 STRING "0"
 -- Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
+-- Retrieval info: PRIVATE: MULT_FACTOR1 NUMERIC "1"
 -- Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "6.14400000"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ1 STRING "4.80000000"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ_MODE1 STRING "1"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT1 STRING "MHz"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_INPUTS_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PHASE_SHIFT0 STRING "0.00000000"
+-- Retrieval info: PRIVATE: PHASE_SHIFT1 STRING "0.00000000"
 -- Retrieval info: PRIVATE: PHASE_SHIFT_STEP_ENABLED_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PHASE_SHIFT_UNIT0 STRING "deg"
+-- Retrieval info: PRIVATE: PHASE_SHIFT_UNIT1 STRING "ps"
 -- Retrieval info: PRIVATE: PLL_ADVANCED_PARAM_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PLL_ARESET_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PLL_AUTOPLL_CHECK NUMERIC "1"
@@ -273,11 +296,14 @@ END SYN;
 -- Retrieval info: PRIVATE: SPREAD_USE STRING "0"
 -- Retrieval info: PRIVATE: SRC_SYNCH_COMP_RADIO STRING "0"
 -- Retrieval info: PRIVATE: STICKY_CLK0 STRING "1"
+-- Retrieval info: PRIVATE: STICKY_CLK1 STRING "1"
 -- Retrieval info: PRIVATE: SWITCHOVER_COUNT_EDIT NUMERIC "1"
 -- Retrieval info: PRIVATE: SWITCHOVER_FEATURE_ENABLED STRING "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: USE_CLK0 STRING "1"
+-- Retrieval info: PRIVATE: USE_CLK1 STRING "1"
 -- Retrieval info: PRIVATE: USE_CLKENA0 STRING "0"
+-- Retrieval info: PRIVATE: USE_CLKENA1 STRING "0"
 -- Retrieval info: PRIVATE: USE_MIL_SPEED_GRADE NUMERIC "0"
 -- Retrieval info: PRIVATE: ZERO_DELAY_RADIO STRING "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
@@ -286,6 +312,10 @@ END SYN;
 -- Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
 -- Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "24"
 -- Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "0"
+-- Retrieval info: CONSTANT: CLK1_DIVIDE_BY NUMERIC "20"
+-- Retrieval info: CONSTANT: CLK1_DUTY_CYCLE NUMERIC "50"
+-- Retrieval info: CONSTANT: CLK1_MULTIPLY_BY NUMERIC "3"
+-- Retrieval info: CONSTANT: CLK1_PHASE_SHIFT STRING "0"
 -- Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 -- Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "31250"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone 10 LP"
@@ -318,7 +348,7 @@ END SYN;
 -- Retrieval info: CONSTANT: PORT_SCANREAD STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: PORT_SCANWRITE STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: PORT_clk0 STRING "PORT_USED"
--- Retrieval info: CONSTANT: PORT_clk1 STRING "PORT_UNUSED"
+-- Retrieval info: CONSTANT: PORT_clk1 STRING "PORT_USED"
 -- Retrieval info: CONSTANT: PORT_clk2 STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: PORT_clk3 STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: PORT_clk4 STRING "PORT_UNUSED"
@@ -337,10 +367,12 @@ END SYN;
 -- Retrieval info: USED_PORT: @clk 0 0 5 0 OUTPUT_CLK_EXT VCC "@clk[4..0]"
 -- Retrieval info: USED_PORT: @inclk 0 0 2 0 INPUT_CLK_EXT VCC "@inclk[1..0]"
 -- Retrieval info: USED_PORT: c0 0 0 0 0 OUTPUT_CLK_EXT VCC "c0"
+-- Retrieval info: USED_PORT: c1 0 0 0 0 OUTPUT_CLK_EXT VCC "c1"
 -- Retrieval info: USED_PORT: inclk0 0 0 0 0 INPUT_CLK_EXT GND "inclk0"
 -- Retrieval info: CONNECT: @inclk 0 0 1 1 GND 0 0 0 0
 -- Retrieval info: CONNECT: @inclk 0 0 1 0 inclk0 0 0 0 0
 -- Retrieval info: CONNECT: c0 0 0 0 0 @clk 0 0 1 0
+-- Retrieval info: CONNECT: c1 0 0 0 0 @clk 0 0 1 1
 -- Retrieval info: GEN_FILE: TYPE_NORMAL pll_2.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL pll_2.ppf TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL pll_2.inc FALSE
